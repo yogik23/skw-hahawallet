2e3cf8f12913f674ef9600e0f227f8b59364f933
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-02-02 06:11:57 +0700

    Update diskw.js

--- skw/diskw.js
+++ skw/diskw.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 
-const welcomeskw = chalk.hex('#9370DB')(`
+const welcomeskw = chalk.hex('#9370DB')(` 
    ███████╗██╗  ██╗██╗    ██╗
    ██╔════╝██║ ██╔╝██║    ██║
    ███████╗█████╔╝ ██║ █╗ ██║
@@ -11,15 +11,15 @@ const welcomeskw = chalk.hex('#9370DB')(`
 
 function displayskw() {
   console.log(welcomeskw);
-  console.log(chalk.hex('#FF4500')(" ╔══════════════════════════════════════════════════════════════╗"));
-  console.log(chalk.hex('#FF8C00')(" ║ ≣  Fitur Autobot by SKW AIRDROP HUNTER                       ║"));
-  console.log(chalk.hex('#FF4500')(" ║══════════════════════════════════════════════════════════════║"));
-  console.log(chalk.hex('#FFD700')(" ║ ➤ 1️⃣  Auto Claim Daily Karma                                ║"));
-  console.log(chalk.hex('#00CED1')(" ║ ➤ 2️⃣  Multi Akun                                            ║"));
-  console.log(chalk.hex('#32CD32')(" ║ ➤ 3️⃣  Jangan Jual Bot ini                                   ║"));
-  console.log(chalk.hex('#1E90FF')(" ║ ➤ 4️⃣  Jangan Dijual                                         ║"));
-  console.log(chalk.hex('#FF4500')(" ╚══════════════════════════════════════════════════════════════╝"));
-  console.log(chalk.hex('#FF6347')("   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"));
+  console.log(chalk.hex('#FF00FF')(" ╔══════════════════════════════════════════════════════════════╗"));
+  console.log(chalk.hex('#FF00FF')(" ║ ≣  Fitur Autobot by SKW AIRDROP HUNTER                       ║"));
+  console.log(chalk.hex('#FF00FF')(" ║══════════════════════════════════════════════════════════════║"));
+  console.log(chalk.hex('#8A2BE2')(" ║ ➤ 1️⃣  Auto Claim Daily Karma                                ║"));
+  console.log(chalk.hex('#8A2BE2')(" ║ ➤ 2️⃣  Multi Akun                                            ║"));
+  console.log(chalk.hex('#8A2BE2')(" ║ ➤ 3️⃣  Jangan Jual Bot ini                                   ║"));
+  console.log(chalk.hex('#8A2BE2')(" ║ ➤ 4️⃣  Jangan Dijual                                         ║"));
+  console.log(chalk.hex('#8A2BE2')(" ╚══════════════════════════════════════════════════════════════╝"));
+  console.log(chalk.hex('#9370DB')("   ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░"));
 }
 
 module.exports = { displayskw };
